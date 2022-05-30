b8f91595f4d8916b810027f52bbb3b325c9899f5
Dan Lampman <dlampman@notchordering.com>
2022-05-30 11:55:03 -0400

    manual patch around multer to 1.4.4-lts.1; cleans up test server

--- test/integration/routes/sample.js
+++ test/integration/routes/sample.js
@@ -1,19 +1,39 @@
-exports.test = {
-    name: 'test-get-person',
-    description: 'retrieve a person by name and age',
+exports.test_get_data = {
+    name: 'test-get-data',
+    description: 'retrieve some data',
     method: 'GET',
-    endpoint: '/test-get-person',
+    endpoint: '/test-get-data',
     version: 'v1',
     inputs: {
-        name: {
-            required: true,
-        },
-        age: {
-            required: true,
-        },
+        'test': {
+            required: true
+        }
     },
     requestBodyOverride: true,
     run: (services, inputs, next) => {
-        next(inputs);
+        next({
+            data: "I'm a little teapot",
+            inputs
+        });
+    }
+}
+
+exports.test_post_data = {
+    name: 'test-post-data',
+    description: 'retrieve some data',
+    method: 'POST',
+    endpoint: '/test-post-data',
+    version: 'v1',
+    inputs: {
+        'test': {
+            required: true
+        }
+    },
+    requestBodyOverride: true,
+    run: (services, inputs, next) => {
+        next({
+            data: "I'm a little teapot",
+            inputs
+        });
     }
 }
